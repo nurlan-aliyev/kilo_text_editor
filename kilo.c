645891826e37cdb2243763c506947834a8f9cc1f
Nurlan Aliyev <nurlan.aliyev58@gmail.com>
2026-08-20 15:23:17 +0200

    move the cursor

--- kilo.c
+++ kilo.c
@@ -20,6 +20,7 @@
 
 struct editorConfig 
 {
+    int cx, cy;
     int screenrows;
     int screencols;
     struct termios orig_termios;
@@ -187,8 +188,11 @@ void editorRefreshScreen()
     abAppend(&ab, "\x1b[H", 3);
     
     editorDrawRows(&ab);
-    
-    abAppend(&ab, "\x1b[H", 3);
+
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+
     abAppend(&ab, "\x1b[?25h", 6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
@@ -214,6 +218,9 @@ void editorProcessKeypress()
 
 void initEditor()
 {
+    E.cx = 0;
+    E.cy = 0;
+
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
         die("getWindowSize");
 }

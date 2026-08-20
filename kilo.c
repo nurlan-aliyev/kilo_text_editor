9e0b7c21feac76cf5e73ad5c5b0ab8c2fa450cc9
Nurlan Aliyev <nurlan.aliyev58@gmail.com>
2026-08-20 13:25:51 +0200

    disables Ctrl-s and Ctrl-q signals

--- kilo.c
+++ kilo.c
@@ -20,6 +20,7 @@ void enableRawMode()
 
     tcgetattr(STDIN_FILENO, &raw);
 
+    raw.c_iflag &= ~(IXON);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);

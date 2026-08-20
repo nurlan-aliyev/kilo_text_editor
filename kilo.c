a00966ee25ac7c2e8995364d99b189e9795206b7
Nurlan Aliyev <nurlan.aliyev58@gmail.com>
2026-08-20 13:52:10 +0200

    make Ctrl-q quit signal

--- kilo.c
+++ kilo.c
@@ -7,6 +7,11 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+
 /*** data ***/
 
 struct termios orig_termios;
@@ -67,7 +72,7 @@ int main()
             printf("%d ('%c')\r\n", c, c);
         }
 
-        if (c == 'q')
+        if (c == CTRL_KEY('q'))
             break;
     }
     return 0;

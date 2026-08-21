61582a8f08764f99095e29494b609ee624881e45
Nurlan Aliyev <nurlan.aliyev58@gmail.com>
2026-08-21 20:10:02 +0200

    handle special keys

--- kilo.c
+++ kilo.c
@@ -24,6 +24,7 @@
 
 enum editorKey
 {
+    BACKSPACE = 127,
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
@@ -543,6 +544,9 @@ void editorProcessKeypress()
 
     switch (c)
     {
+        case '\r':
+            /* TODO */
+            break;
         case CTRL_KEY('q'):
             write(STDOUT_FILENO, "\x1b[2J]", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
@@ -555,6 +559,12 @@ void editorProcessKeypress()
         
         case END_KEY:
             if (E.cy < E.numrows) E.cx = E.row[E.cy].size;
+            break;
+
+        case BACKSPACE:
+        case CTRL_KEY('h'):
+        case DEL_KEY:
+            /* TODO */
             break;
 
         case PAGE_UP:
@@ -580,6 +590,10 @@ void editorProcessKeypress()
         case ARROW_LEFT:
         case ARROW_RIGHT:
             editorMoveCursor(c);
+            break;
+
+        case CTRL_KEY('l'):
+        case '\x1b':
             break;
 
         default:

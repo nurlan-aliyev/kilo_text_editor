e894e58088b76d750364a2f69c9a8472ac2d57d5
Nurlan Aliyev <nurlan.aliyev58@gmail.com>
2026-08-22 12:01:24 +0200

    make digits colorful

--- kilo.c
+++ kilo.c
@@ -674,6 +674,21 @@ void editorDrawRows(struct abuf *ab)
                 len = 0;
             if (len > E.screencols)
                 len = E.screencols;
+            char *c = &E.row[filerow].render[E.coloff];
+            int j;
+            for (j = 0; j < len; j++)
+            {
+                if (isdigit(c[j]))
+                {
+                    abAppend(ab, "\x1b[31m", 5);
+                    abAppend(ab, &c[j], 1);
+                    abAppend(ab, "\x1b[39m", 5);
+                }
+                else
+                {
+                    abAppend(ab, &c[j], 1);
+                }
+            }
             abAppend(ab, &E.row[filerow].render[E.coloff], len);
         }
 
